1135a30f2e63843b4f2d3c46351556b23cb23a55
Timur <v.timur8484@gmail.com>
2018-11-03 16:15:22 +0500

    Update Metrika and VK script

--- views/_tpl.php
+++ views/_tpl.php
@@ -32,36 +32,46 @@
 
 <!-- Yandex.Metrika counter -->
 <script type="text/javascript" > 
-(function (d, w, c) { 
-(w[c] = w[c] || []).push(function() { 
-try { 
-w.yaCounter50940317 = new Ya.Metrika2({ 
-id:50940317, 
-clickmap:true, 
-trackLinks:true, 
-accurateTrackBounce:true, 
-webvisor:true 
-}); 
-} catch(e) { } 
-}); 
-
-var n = d.getElementsByTagName("script")[0], 
-s = d.createElement("script"), 
-f = function () { n.parentNode.insertBefore(s, n); }; 
-s.type = "text/javascript"; 
-s.async = true; 
-s.src = "https://mc.yandex.ru/metrika/tag.js"; 
-
-if (w.opera == "[object Opera]") { 
-d.addEventListener("DOMContentLoaded", f, false); 
-} else { f(); } 
-})(document, window, "yandex_metrika_callbacks2"); 
+ (function (d, w, c) { 
+    (w[c] = w[c] || []).push(function() { 
+       try { 
+          w.yaCounter50940317 = new Ya.Metrika2({ 
+             id:50940317, 
+             clickmap:true, 
+             trackLinks:true, 
+             accurateTrackBounce:true, 
+             webvisor:true 
+          }); 
+       } catch(e) { } 
+    }); 
+    var n = d.getElementsByTagName("script")[0], 
+        s = d.createElement("script"), 
+        f = function () { n.parentNode.insertBefore(s, n); }; 
+    s.type = "text/javascript"; 
+    s.async = true; 
+    s.src = "https://mc.yandex.ru/metrika/tag.js"; 
+    if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); }
+    else { f(); } 
+ })(document, window, "yandex_metrika_callbacks2");
 </script> 
 <noscript><div><img src="https://mc.yandex.ru/watch/50940317" style="position:absolute; left:-9999px;" alt="" /></div></noscript> 
 <!-- / Yandex.Metrika counter -->
 
 <!-- VK Retargeting -->
-<script type="text/javascript">!function(){var t=document.createElement("script");t.type="text/javascript",t.async=!0,t.src="https://vk.com/js/api/openapi.js?159",t.onload=function(){VK.Retargeting.Init("VK-RTRG-300151-ezlL5"),VK.Retargeting.Hit()},document.head.appendChild(t)}();</script><noscript><img src="https://vk.com/rtrg?p=VK-RTRG-300151-ezlL5" style="position:fixed; left:-999px;" alt=""/></noscript>
+<script type="text/javascript">
+(function() {
+  var t = document.createElement("script");
+  t.type = "text/javascript";
+  t.async = true;
+  t.src = "https://vk.com/js/api/openapi.js?159";
+  t.onload = function() {
+    VK.Retargeting.Init("VK-RTRG-300151-ezlL5");
+    VK.Retargeting.Hit();
+  };
+  document.head.appendChild(t);
+})();
+</script>
+<noscript><img src="https://vk.com/rtrg?p=VK-RTRG-300151-ezlL5" style="position:fixed; left:-999px;" alt=""/></noscript>
 <!-- / VK Retargeting -->
 
 
